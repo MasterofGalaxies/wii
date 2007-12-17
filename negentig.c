5058a9f0800dcc95c78f4c2641121f7c4578e056
Segher Boessenkool <segher@kernel.crashing.org>
2007-12-17 02:49:39 +0100

    Add copyright and licensing header.

--- negentig.c
+++ negentig.c
@@ -1,3 +1,6 @@
+// Copyright 2007  Segher Boessenkool  <segher@kernel.crashing.org>
+// Licensed under the terms of the GNU GPL, version 2
+
 #include <openssl/aes.h>
 #include <sys/types.h>
 #include <sys/stat.h>
