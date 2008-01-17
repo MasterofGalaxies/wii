52e61dec3e0dff5160e16c6c1608914311153ee7
Segher Boessenkool <segher@schijntje.local>
2008-01-17 12:14:25 +0100

    Add license URL

--- negentig.c
+++ negentig.c
@@ -1,5 +1,6 @@
 // Copyright 2007,2008  Segher Boessenkool  <segher@kernel.crashing.org>
 // Licensed under the terms of the GNU GPL, version 2
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 #include <openssl/aes.h>
 #include <sys/types.h>
