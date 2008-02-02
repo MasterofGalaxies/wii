3d3f653eb007a0e6326c184668f1faebb45f2866
Segher Boessenkool <segher@kernel.crashing.org>
2008-02-02 15:56:05 +0100

    "Whoops".

--- tpl2ppm.c
+++ tpl2ppm.c
@@ -4,21 +4,9 @@
 
 #include <stdio.h>
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
+#include "tools.h"
 
 static u8 buf[5000000]; // yeah yeah, static buffer, whatever
-
-static u16 be16(u8 *p)
-{
-	return (p[0] << 8) | p[1];
-}
-
-static u32 be32(u8 *p)
-{
-	return (p[0] << 24) | (p[1] << 16) | (p[2] << 8) | p[3];
-}
 
 static void i4(int w, int h, int o, char *name)
 {
